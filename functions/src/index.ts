478798a2d5019fdecfa258902d4a02369292dd9a
chandai <guy1nthecha1r2049@gmail.com>
2023-07-28 23:30:20 +0900

    simple api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,27 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+admin.initializeApp();
+export const getBooks = onRequest(async (request, response) => {
+    try {
+        // firestoreのインスタンスを取得
+        const db = admin.firestore();
+
+        //インスタンスからコレクションを取得
+        const ref = await db.collection('books').get();
+        response.send(ref.docs);
+    } catch (e) {
+        console.error(e);
+        response.status(500).send(e);
+    }
+});
