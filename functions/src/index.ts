95db170159e45d15ee2b2ab762cb2d71c40e7bc5
chandai <guy1nthecha1r2049@gmail.com>
2023-07-28 23:40:51 +0900

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 
@@ -15,21 +15,21 @@ import * as admin from "firebase-admin";
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
+  logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
 
 admin.initializeApp();
 export const getBooks = onRequest(async (request, response) => {
-    try {
-        // firestoreのインスタンスを取得
-        const db = admin.firestore();
+  try {
+    // firestoreのインスタンスを取得
+    const db = admin.firestore();
 
-        //インスタンスからコレクションを取得
-        const ref = await db.collection('books').get();
-        response.send(ref.docs);
-    } catch (e) {
-        console.error(e);
-        response.status(500).send(e);
-    }
+    //インスタンスからコレクションを取得
+    const ref = await db.collection("books").get();
+    response.send(ref.docs);
+  } catch (e) {
+    console.error(e);
+    response.status(500).send(e);
+  }
 });
