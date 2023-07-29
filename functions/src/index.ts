f801223ef644a4a2adcb051de601abafa3eff82c
chandai <guy1nthecha1r2049@gmail.com>
2023-07-29 13:19:13 +0900

    createPostFood

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,20 +7,20 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from "firebase-functions/v2/https";
+import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
 
 admin.initializeApp();
-export const getBooks = onRequest(async (request, response) => {
+export const getBooks = functions.https.onRequest(async (request, response) => {
   try {
     // firestoreのインスタンスを取得
     const db = admin.firestore();
@@ -31,5 +31,53 @@ export const getBooks = onRequest(async (request, response) => {
   } catch (e) {
     console.error(e);
     response.status(500).send(e);
+  }
+});
+
+const validatePostFoodParamsSchema = (params: any) => {
+  const hasName = "name" in params;
+  const hasExpiration = "expiration" in params;
+  const hasLocation = "location" in params;
+  const hasImageString = "imageString" in params;
+
+  return hasName && hasExpiration && hasLocation && hasImageString;
+};
+
+export const postFood = functions.https.onCall(async (data, context) => {
+  if (validatePostFoodParamsSchema(data)) {
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "invalid paramteres"
+    );
+  }
+
+  const { name, expiration, location, imageString } = data;
+
+  try {
+    const db = admin.firestore();
+    await db
+      .collection("foods")
+      .add({
+        name: name,
+        expiration: expiration,
+        location: location,
+        imageString: imageString,
+      })
+      .then((documentReference) => {
+        logger.info(`Added document with name: ${documentReference.id}`);
+        return {
+          documentId: documentReference.id,
+          name: name,
+          expiration: expiration,
+          location: location,
+          imageString: imageString,
+        };
+      });
+  } catch (e) {
+    throw new functions.https.HttpsError(
+      "internal",
+      "internal server error",
+      e
+    );
   }
 });
